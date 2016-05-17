20e33d54c8363e81ede81ee05b5a9ce8264dadc0
Mr Client <arashnomiri@gmail.com>
2016-05-17 21:30:58 +0000

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -17,7 +17,7 @@ local function chat_list(msg)
     local data = load_data(_config.moderation.data)
         local groups = 'groups'
         if not data[tostring(groups)] then
-                return 'No groups at the moment'
+                return 'این گروه گروه من نیست'
         end
         local message = 'List of Groups:\n*Use /join (ID) to join*\n\n '
         for k,v in pairs(data[tostring(groups)]) do
@@ -42,13 +42,13 @@ local function run(msg, matches)
 	 local data = load_data(_config.moderation.data)
     if matches[1] == 'join' and data[tostring(matches[2])] then
         if is_banned(msg.from.id, matches[2]) then
-	    return 'You are banned.'
+	    return 'شما بن هستید.'
 	 end
       if is_gbanned(msg.from.id) then
-            return 'You are globally banned.'
+            return 'شما از تمام گروه های من بن هستید.'
       end
       if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
-        return 'Group is private.'
+        return 'گروه خصوصی هست.'
       end
           local chat_id = "chat#id"..matches[2]
           local user_id = "user#id"..msg.from.id
@@ -57,7 +57,7 @@ local function run(msg, matches)
 	  return "Added you to chat:\n\n👥"..group_name.." (ID:"..matches[2]..")"
         elseif matches[1] == 'join' and not data[tostring(matches[2])] then
 		
-         	return "Chat not found."
+         	return "گروه یافت نشد."
         end
      if matches[1] == 'chats'then
        if is_admin(msg) and msg.to.type == 'chat' then
